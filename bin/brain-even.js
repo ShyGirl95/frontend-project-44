1afc10aa01f620167bc6eadd04f0b6e19f793d60
student <hawaii_paradise@mail.ru>
2023-03-15 14:05:17 +0300

    brain-even-rework-1

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const gameEven = (something) => {
+const gameEven = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
