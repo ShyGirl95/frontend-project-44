5a3df15530b32557b46509e51ad39b137f17b0d1
student <hawaii_paradise@mail.ru>
2023-05-26 15:46:35 +0300

    brain-prime_rework

--- src/brain-prime.js
+++ src/brain-prime.js
@@ -1,10 +1,17 @@
 import readlineSync from 'readline-sync';
 import randomNumber from './random.js';
 
+const isPrime = (num) => {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+};
+
 const gamePrime = () => {
   const roundsToWin = 3;
-  const primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41,
-    43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
@@ -15,10 +22,9 @@ const gamePrime = () => {
     const potentialPrimeNumber = randomNumber();
     console.log(`${question}${potentialPrimeNumber}`);
     const yourAnwser = readlineSync.question('Your anwser: ');
-    const isPrime = primeNumbers.includes(potentialPrimeNumber);
-    if (isPrime === true) {
+    if (isPrime(potentialPrimeNumber)) {
       rightAnswer = 'yes';
-    } else if (isPrime === false) {
+    } else {
       rightAnswer = 'no';
     }
     if (rightAnswer !== yourAnwser) {
